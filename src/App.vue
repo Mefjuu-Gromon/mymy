ae4a2d3d982e44d19045089a2fd935daa43b1d00
Mateusz <mateuszskrzypiec12@gmail.com>
2020-02-29 20:30:18 +0100

    The first view styles have been added

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <template>
   <div class="app">
     <div class='nav'>
-      <router-link to='/home'>GromOn</router-link>
-      <router-link to='/about'>About</router-link>
     </div>
   <router-view />
   </div>
 </template>
 
-<style lang='scss' scoped>
-
-
-</style>
+<style lang='scss' scoped> </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
-    <h1>Hejka Naklejka!</h1>
+    <div class='firstView'>
+      <h1 class='HeroStyles-Text pos01'> Hejka</h1>
+      <h1 class='HeroStyles-Text pos02'> Naklejka!</h1>
+    </div>
   </div>
 </template>
 
@@ -11,3 +14,53 @@ export default {
   name: 'Home',
 };
 </script>
+
+<style lang='scss' scoped>
+*{
+  margin: 0;
+  padding:0;
+}
+
+body{
+  margin:0;
+  padding:0;
+  border: 0;
+}
+
+.firstView{
+  display:flex;
+  width:100%;
+  height:98vh;
+  font-family: 'Muli', ;
+}
+.pos01{
+  @media(min-width: 1350px){
+    margin-top:30%;
+    margin-left: 59%;
+  }
+
+  @media(min-width: 300px){
+    margin-top: 60%;
+  }
+  @media(min-width: 1350px){
+    margin-top:20%;
+    font-size: 5.4rem;
+  }
+
+}
+
+.pos02{
+  @media(min-width: 1050px){
+    margin-top:30%;
+  }
+
+  @media(min-width: 300px){
+    margin-top: 60%;
+  }
+  @media(min-width: 1350px){
+    margin-top:25%;
+    font-size: 5.4rem;
+  }
+}
+
+</style>
